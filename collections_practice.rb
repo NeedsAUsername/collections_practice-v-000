3448847c68e55050a7bfd96b8bc0e04095eb66e1
Kevin Chi <kevinchi118@gmail.com>
2018-06-14 16:38:26 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,4 +8,12 @@ end
 
 def sort_array_char_count(array)
     return array.sort{|x, y| x.length <=> y.length}
-end 
+end
+
+def sort_swap_elements(array)
+    value_2 = array[1]
+    value_3 = array[2]
+    array[1] = value_2
+    array[2] = value_3
+    return array
+end
