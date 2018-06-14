e4774a33e738eecc955440a179a95d2fc0ec71dd
Kevin Chi <kevinchi118@gmail.com>
2018-06-14 16:36:16 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,11 @@
+def sort_array_asc(array)
+    return array.sort
+end
 
+def sort_array_desc(array)
+    return array.sort{|x, y| y <=> x}
+end
+
+def sort_array_char_count(array)
+    return array.sort{|x, y| x.length <=> y.length}
+end 
